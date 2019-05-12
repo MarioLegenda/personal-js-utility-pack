1f83631d1947af3bbbccf19f1e4bfc0c925147b4
mario <likearollingstone199@gmail.com>
2019-05-12 21:02:53 +0100

    Created range function

--- src/misc.js
+++ src/misc.js
@@ -15,7 +15,10 @@ function is(type, val) {
 
     if (type === 'float') {
         return val === +val && val !== (val | 0);
+    }
 
+    if (type.toLowerCase() === 'nan') {
+        return val !== val;
     }
 
     return Object.prototype.toString.call(val) === res;
@@ -25,10 +28,46 @@ function ucFirst(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
+function range(start, end, step) {
+    if (!is('number', start)) throw new Error('range() invalid argument. First argument must be a number');
+    if (!is('number', end)) throw new Error('range() invalid argument. Second argument must be a number');
+
+    if (end < start) throw new Error(`range() invalid argument. 'end' cannot be lower than 'start'`);
+    if (start === end) return [];
+
+    let iterations = end - start;
+    let isNegative = Math.sign(start);
+
+    if (!step) {
+        return [...Array(iterations + 1).keys()].map(i => i + start);
+    }
+
+    const result = [];
+
+    while((iterations--) >= 0) {
+        if (step) {
+            if (isNegative === -1) {
+                if ((start % step) === 0) {
+                    result.push(start);
+                }
+            } else if (start !== 0 && (start % step) === 0) {
+                result.push(start);
+            }
+        } else {
+            result.push(start);
+        }
+
+        ++start;
+    }
+
+    return result;
+}
+
 function Misc() {
     this.inHttpStatusRange = inHttpRange;
     this.is = is;
     this.ucFirst = ucFirst;
+    this.range = range;
 }
 
 module.exports = new Misc();
--- tests/unit/toolbox.spec.js
+++ tests/unit/toolbox.spec.js
@@ -4,33 +4,85 @@ const toolbox = require('../../src/toolbox');
 const urlService = require('url');
 
 const it = mocha.it;
+const describe = mocha.describe;
 const expect = chai.expect;
 
-it('should pass all misc functions', () => {
-    const boolTrue = true;
-    const boolFalse = false;
-    const string = 'string';
-    const number = 1;
-    const float = 1.1;
-    const stringObj = new String('mile');
-    const boolObject = new Boolean(true);
-    const array = [];
-    const obj = {};
-    const objectInst = new Object();
-    const func = new Function();
+describe('testing all misc functions # ', () => {
+    it('is() function should pass all checks', () => {
+        const boolTrue = true;
+        const boolFalse = false;
+        const string = 'string';
+        const number = 1;
+        const float = 1.1;
+        const stringObj = new String('mile');
+        const boolObject = new Boolean(true);
+        const array = [];
+        const obj = {};
+        const objectInst = new Object();
+        const func = new Function();
+        const nan = NaN;
 
-    expect(toolbox.misc.is('boolean', boolTrue)).to.be.true;
-    expect(toolbox.misc.is('boolean', boolFalse)).to.be.true;
-    expect(toolbox.misc.is('string', string)).to.be.true;
-    expect(toolbox.misc.is('integer', number)).to.be.true;
-    expect(toolbox.misc.is('float', float)).to.be.true;
-    expect(toolbox.misc.is('string', stringObj)).to.be.true;
-    expect(toolbox.misc.is('boolean', boolObject)).to.be.true;
-    expect(toolbox.misc.is('array', array)).to.be.true;
-    expect(toolbox.misc.is('object', obj)).to.be.true;
-    expect(toolbox.misc.is('object', objectInst)).to.be.true;
-    expect(toolbox.misc.is('function', func)).to.be.true;
+        expect(toolbox.misc.is('boolean', boolTrue)).to.be.true;
+        expect(toolbox.misc.is('boolean', boolFalse)).to.be.true;
+        expect(toolbox.misc.is('string', string)).to.be.true;
+        expect(toolbox.misc.is('integer', number)).to.be.true;
+        expect(toolbox.misc.is('float', float)).to.be.true;
+        expect(toolbox.misc.is('string', stringObj)).to.be.true;
+        expect(toolbox.misc.is('boolean', boolObject)).to.be.true;
+        expect(toolbox.misc.is('array', array)).to.be.true;
+        expect(toolbox.misc.is('object', obj)).to.be.true;
+        expect(toolbox.misc.is('object', objectInst)).to.be.true;
+        expect(toolbox.misc.is('function', func)).to.be.true;
+        expect(toolbox.misc.is('nan', nan)).to.be.true;
+    });
+
+    it('should create a range of numbers', () => {
+        let range = toolbox.misc.range(1, 10);
+
+        expect(range.length).to.be.equal(10);
+        index = 1;
+        for (const item of range) {
+            expect(item).to.be.equal(index++);
+        }
+
+        range = toolbox.misc.range(1, 10, 2);
+
+        expect(range.length).to.be.equal(5);
+
+        index = 2;
+        for (const item of range) {
+          expect(item).to.be.equal(index);
+
+          index += 2;
+        }
+
+        range = toolbox.misc.range(0, 10, 2);
+
+        index = 2;
+        for (const item of range) {
+            expect(item).to.be.equal(index);
+
+            index += 2;
+        }
+
+        range = toolbox.misc.range(-5, 10);
+
+        index = -5;
+        for (const item of range) {
+            expect(item).to.be.equal(index++);
+        }
+
+        range = toolbox.misc.range(-5, 10, 2);
+
+        index = -4;
+        for (const item of range) {
+          expect(item).to.be.equal(index);
+
+          index += 2;
+        }
+    });
 })
+
 
 it('should parse the url into domain parts', () => {
     const url = 'https://developer.mozilla.org/en-US/';
